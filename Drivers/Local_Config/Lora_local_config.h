30871c0eb1933aad299a39b07f43452097d5fe12
Paseka <bazhen.paseka@gmail.com>
2022-09-28 21:44:28 +0300

    v4.06.0

--- Drivers/Local_Config/Lora_local_config.h
+++ Drivers/Local_Config/Lora_local_config.h
@@ -24,7 +24,7 @@
 **************************************************************************
 */
 
-	#define 	SOFT_VERSION 		4050
+	#define 	SOFT_VERSION 		4060
 	#define		MASTER
 	#define		ADC_COEFFICIENT		8233	//	for 2R
 
